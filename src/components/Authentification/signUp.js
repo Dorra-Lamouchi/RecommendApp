23201c7f577e4f123f4e2a26035e5a6d4e6d2d75
abdelkader <komti316@gmail.com>
2021-05-16 15:18:44 +0100

    test

--- src/components/Authentification/signUp.js
+++ src/components/Authentification/signUp.js
@@ -12,7 +12,7 @@ export default function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const {signup} = useAuth()
+  const { signup } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const history = useHistory()
@@ -23,10 +23,10 @@ export default function Signup() {
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match")
     }
-    
+
     if ((parseInt(phoneRef.current.value).toString().length !== phoneRef.current.value.length) || (phoneRef.current.value.length !== 8)) {
-        return setError("Phone number invalid")
-      }
+      return setError("Phone number invalid")
+    }
 
     if (passwordRef.current.value.length < 6) {
       return setError("Password too short")
@@ -46,44 +46,44 @@ export default function Signup() {
   return (
     <div>
       <NavBar />
-    <div id="container" className="d-flex " >
-      <Card id="Card">
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form id="Form" onSubmit={handleSubmit}>
-            <Form.Group id="name">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" ref={nameRef} required />
-            </Form.Group>
-            <Form.Group id="phone">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="text" ref={phoneRef} required />
-            </Form.Group>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button id="Button" disabled={loading} className="w-50" type="submit">
-              Sign Up
+      <div id="container" className="d-flex " >
+        <Card id="Card">
+          <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form id="Form" onSubmit={handleSubmit}>
+              <Form.Group id="name">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" ref={nameRef} required />
+              </Form.Group>
+              <Form.Group id="phone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type="text" ref={phoneRef} required />
+              </Form.Group>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+              </Form.Group>
+              <Button id="Button" disabled={loading} className="w-50" type="submit">
+                Sign Up
             </Button>
-            <div className="w-100 text-center mt-2">
-              Already have an account? <Link to="/signin">Log In</Link>
-            </div>
-          </Form>
-         
-        </Card.Body>
-      </Card>
-      
-    </div>
+              <div className="w-100 text-center mt-2">
+                Already have an account? <Link to="/signin">Log In</Link>
+              </div>
+            </Form>
+
+          </Card.Body>
+        </Card>
+
+      </div>
     </div>
   )
 }
