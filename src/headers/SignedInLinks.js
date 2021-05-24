fa27e648366e4b2a41bde41d856cf63d583cda45
Wael Youssfi <youssfi.wael1997@gmail.com>
2021-05-24 04:35:13 +0100

    ajout candidature et acceptation form

--- src/headers/SignedInLinks.js
+++ src/headers/SignedInLinks.js
@@ -1,42 +1,72 @@
-import React from 'react'
-import { Link, NavLink, useHistory } from 'react-router-dom'
-import {useAuth} from '../components/Authentification/AuthContext'
-import './nav.css'
+import React from "react";
+import { Link, NavLink, useHistory } from "react-router-dom";
+import { useAuth } from "../components/Authentification/AuthContext";
+import "./nav.css";
 
 const SignedInLinks = () => {
   const stylelink = {
-    textDecoration : 'none',
-    color : 'white ', 
-  }
+    textDecoration: "none",
+    color: "white ",
+  };
 
-  const { logout, currentUser } = useAuth()
-  const history = useHistory()
+  const { logout, currentUser } = useAuth();
+  const history = useHistory();
 
   async function handleLogout() {
-    
     try {
-      await logout()
-      history.push("/signin")
-    } catch {
-      
-    }
+      await logout();
+      history.push("/signin");
+    } catch {}
   }
 
   return (
     <div>
       <ul id="menuLeft">
-        <li><Link style={stylelink} to="/">Accueil</Link></li>
-        <li><Link style={stylelink} to="/Stages">Stages</Link></li>  
-        <li><Link style={stylelink} to="/formations">Formations</Link></li>  
-        <li><Link  style={stylelink} to="/emploi">Offres d'Emplois</Link></li>  
-        <li><Link  style={stylelink} to="/contact">Contact</Link></li>
+        <li>
+          <Link style={stylelink} to="/">
+            Accueil
+          </Link>
+        </li>
+        <li>
+          <Link style={stylelink} to="/Stages">
+            Stages
+          </Link>
+        </li>
+        <li>
+          <Link style={stylelink} to="/formations">
+            Formations
+          </Link>
+        </li>
+        <li>
+          <Link style={stylelink} to="/emploi">
+            Offres d'Emplois
+          </Link>
+        </li>
+        <li>
+          <Link style={stylelink} to="/contact">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link style={stylelink} to="/chatroom">
+            Chatroom
+          </Link>
+        </li>
       </ul>
       <ul className="right">
-        <li><NavLink variant="link" to="/signin" onClick={handleLogout}>Log Out</NavLink></li>
-        <li><NavLink to='/' className="btn-floating pink lighten-1 text-center">{currentUser.email.charAt(0)}</NavLink></li>
+        <li>
+          <NavLink variant="link" to="/signin" onClick={handleLogout}>
+            Log Out
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="btn-floating pink lighten-1 text-center">
+            {currentUser.email.charAt(0)}
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SignedInLinks
+export default SignedInLinks;
