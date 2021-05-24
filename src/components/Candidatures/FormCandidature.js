ff3f6afbe49799779d2ad9b9bba565cfbc2488fb
abdelkader <komti316@gmail.com>
2021-05-24 23:18:04 +0100

    test

--- src/components/Candidatures/FormCandidature.js
+++ src/components/Candidatures/FormCandidature.js
@@ -3,7 +3,7 @@ import React from 'react'
 const FormCandidature = (props) => {
     return (
         <div>
-            <h1>Ici Le Formulaire De Candidature avec id= {props.match.params.id}</h1>
+            <h1>Ici Le Formulaire De Candidature avec id= {props.match.params.id}   cc</h1>
         </div>
     )
 }
