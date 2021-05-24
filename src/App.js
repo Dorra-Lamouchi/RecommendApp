81ffcfecd33e040c0c658807c5d32c8f117aa360
Wael Youssfi <youssfi.wael1997@gmail.com>
2021-05-24 07:41:52 +0100

    Hangup+acceptation+candidature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 //import Listpub from './listepublication'
 //import Appp from './components/test'
 
-import NavBar from './headers/nav';
-import Footer from './headers/foot'
-import React from 'react';
+import NavBar from "./headers/nav";
+import Footer from "./headers/foot";
+import React from "react";
 // import './components/Formations/Formation.css'
 //import Search from './components/search'
 import Formations from "./components/categories/formations";
@@ -14,88 +14,126 @@ import Formations from "./components/categories/formations";
 import Stages from "./components/categories/stages";
 import Emploi from "./components/categories/emploi";
 //import Form from 'react-bootstrap/Form'
-import Contact from './components/contact'
-import Accueil from './components/accueil'
-import Affichemploi from './components/categories/afficheemploi'
-import Affichestage from './components/categories/affichestage'
-import Affichedetail from './components/categories/afficheformation'
-import addemploi from './components/Emploi/Emploi'
-import addformation from './components/Formations/Formation'
-import affichercandidature from './components/Candidatures/Afficher'
-import FormCandidature from './components/Candidatures/FormCandidature'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from "./components/contact";
+import Accueil from "./components/accueil";
+import Affichemploi from "./components/categories/afficheemploi";
+import Affichestage from "./components/categories/affichestage";
+import Affichedetail from "./components/categories/afficheformation";
+import addemploi from "./components/Emploi/Emploi";
+import addformation from "./components/Formations/Formation";
+import affichercandidature from "./components/Candidatures/Afficher";
+import FormCandidature from "./components/Candidatures/FormCandidature";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
-import SignUp from './components/Authentification/signUp';
-import SignIn from './components/Authentification/signIn';
-import { AuthProvider } from './components/Authentification/AuthContext';
-import PrivateRoute from './components/Authentification/PrivateRoute';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignUp from "./components/Authentification/signUp";
+import SignIn from "./components/Authentification/signIn";
+import { AuthProvider } from "./components/Authentification/AuthContext";
+import PrivateRoute from "./components/Authentification/PrivateRoute";
 import Preference from "./components/categories/Preferences";
-import Testaffichage from "./components/categories/testaffichage"
+import Testaffichage from "./components/categories/testaffichage";
 import RecruterSignup from "./components/Authentification/RecruterSignup";
-
+import Acceptation from "./components/Candidatures/Acceptation";
+import ChatRoom from "./components/ChatRoom/ChatRoom";
 
 class App extends React.Component {
+  render() {
+    return (
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <div class="app-main__outer">
+              <div class="app-main__inner"></div>
+              <div class="tab-content">
+                <div
+                  class="tab-pane tabs-animation fade show active"
+                  id="tab-content-0"
+                  role="tabpanel"
+                >
+                  <div class="row-9">
+                    <NavBar />
 
+                    <Switch>
+                      <Route path="/signup" component={SignUp}></Route>
+                      <Route
+                        path="/recrutersignup"
+                        component={RecruterSignup}
+                      ></Route>
+                      <Route path="/signin" component={SignIn}></Route>
+                    </Switch>
 
- 
-render(){
-  return (
-    <AuthProvider>
-    <Router>
-    <div className="App">
-       <div class="app-main__outer">
-        <div class="app-main__inner"></div>
-        <div class="tab-content">
-        <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
-          <div class="row-9" >
+                    <Route path="/" exact component={Accueil}></Route>
+                    <Route path="/emploi" exact component={Emploi}></Route>
+                    <Route
+                      path="/Formations"
+                      exact
+                      component={Formations}
+                    ></Route>
+                    <Route path="/Stages" exact component={Stages}></Route>
+                    <Route path="/contact" exact component={Contact}></Route>
+                    <PrivateRoute
+                      path="/afficheformation/:id"
+                      exact
+                      component={Affichedetail}
+                    ></PrivateRoute>
+                    <PrivateRoute
+                      path="/affichestage/:id"
+                      exact
+                      component={Affichestage}
+                    ></PrivateRoute>
+                    <PrivateRoute
+                      path="/afficheemploi/:id"
+                      exact
+                      component={Affichemploi}
+                    ></PrivateRoute>
 
-    <NavBar/>
-    
-      <Switch>
-        <Route path="/signup" component={SignUp}></Route>
-        <Route path="/recrutersignup" component={RecruterSignup}></Route>
-        <Route path="/signin" component={SignIn}></Route>
-      </Switch>
-    
- 
-    <Route path="/" exact component={Accueil}></Route>
-    <Route path="/emploi" exact component={Emploi}></Route>
-    <Route path="/Formations" exact component={Formations}></Route>
-    <Route path="/Stages" exact component={Stages}></Route>
-    <Route path="/contact" exact component={Contact}></Route>
-    <PrivateRoute path="/afficheformation/:id" exact component={Affichedetail}></PrivateRoute>
-    <PrivateRoute path="/affichestage/:id" exact component={Affichestage}></PrivateRoute>
-    <PrivateRoute path="/afficheemploi/:id" exact component={Affichemploi}></PrivateRoute>
-    
+                    <Route
+                      path="/ajoutemploi"
+                      exact
+                      component={addemploi}
+                    ></Route>
+                    <Route
+                      path="/ajoutformation"
+                      exact
+                      component={addformation}
+                    ></Route>
+                    <Route
+                      path="/affichecondidature"
+                      exact
+                      component={affichercandidature}
+                    ></Route>
+                    <Route
+                      path="/preference"
+                      exact
+                      component={Preference}
+                    ></Route>
+                    <Route
+                      path="/formcandidature/:id"
+                      exact
+                      component={FormCandidature}
+                    ></Route>
+                    <Route
+                      path="/Testaffichage/:id"
+                      exact
+                      component={Testaffichage}
+                    ></Route>
+                    <Route
+                      path="/acceptation/:id"
+                      exact
+                      component={Acceptation}
+                    ></Route>
+                    <Route path="/chatroom" exact component={ChatRoom}></Route>
 
-    <Route path="/ajoutemploi" exact component={addemploi}></Route>
-    <Route path="/ajoutformation" exact component={addformation}></Route>
-    <Route path="/affichecondidature" exact component={affichercandidature}></Route>
-    <Route path="/preference" exact component={Preference}></Route>
-  <Route path="/formcandidature/:id" exact component={FormCandidature}></Route>
-   <Route path="/Testaffichage/:id" exact component={Testaffichage}></Route>
-
-
-    <Footer />
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-  </Router>
-  </AuthProvider>
-  );
+                    <Footer />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Router>
+      </AuthProvider>
+    );
+  }
 }
-
-}
-
-
 
 export default App;
--- src/components/Candidatures/FormCandidature.js
+++ src/components/Candidatures/FormCandidature.js
@@ -1,11 +1,146 @@
-import React from 'react'
+// // import React from 'react'
+
+// // const FormCandidature = (props) => {
+// //     return (
+// //         <div>
+// //             <h1>Ici Le Formulaire De Candidature avec id= {props.match.params.id}</h1>
+// //         </div>
+// //     )./FormUI/Select
+// // }
+
+// // export default FormCandidature
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import TextFieldComponent from "./FormUI/textFieldComponent";
+import ButtonComponent from "./FormUI/buttonComponent";
+import firebaseDb from "../../firebase";
+import { Grid, makeStyles, Container, Typography } from "@material-ui/core";
+import Select from "./FormUi/Select";
+import FileUploader from "./FormUI/ileUploader";
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required!"),
+  lastName: Yup.string().required("Required!"),
+  postTitle: Yup.string().required("Required!"),
+  email: Yup.string().email("Invalid email!").required("Required!"),
+  experience: Yup.number(),
+  studyLevel: Yup.string().required("Required!"),
+  // resume: Yup.object().required("Required!"),
+});
+
+const studyLevels = {
+  "No degree": "No degree",
+  Bachelor: "Bachelor",
+  Master: "Master",
+  Phd: "Phd",
+};
 
 const FormCandidature = (props) => {
-    return (
-        <div>
-            <h1>Ici Le Formulaire De Candidature avec id= {props.match.params.id}</h1>
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default FormCandidature
+  const resumeStorageRef = firebaseDb.storage().ref("Resume/");
+
+  const candidatureRef = firebaseDb.firestore().collection("Candidature");
+
+  let title;
+
+  const PtitleRef = firebaseDb
+    .firestore()
+    .collection("OffresEmploi")
+    .doc(props.match.params.id);
+  PtitleRef.get().then((doc) => {
+    title = doc.data();
+  });
+
+  const INITIAL_FORM_STATE = {
+    firstName: "",
+    lastName: "",
+    postTitle: title,
+    experience: "",
+    studyLevel: "",
+    email: "",
+    resumePdf: "",
+  };
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{ ...INITIAL_FORM_STATE }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values) => {
+                console.log(values);
+
+                candidatureRef.doc(values.postTitle).set({
+                  title: values.postTitle,
+                  CV: values.resumePdf.name.toLowerCase(),
+                  Niveau: values.studyLevel,
+                  Nom: values.lastName,
+                  Prenom: values.firstName,
+                  Experience: values.experience,
+                });
+
+                const file = values.resumePdf;
+                const fileRef = resumeStorageRef.child(file.name.toLowerCase());
+                fileRef.put(file);
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography>Job details</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextFieldComponent name="postTitle" label="Job Post *" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography>Your details</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextFieldComponent name="firstName" label="First Name *" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextFieldComponent name="lastName" label="Last Name *" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextFieldComponent name="email" label="E-mail *" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextFieldComponent
+                      name="experience"
+                      label="Experience (years)"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Select
+                      name="studyLevel"
+                      label="Study Level *"
+                      options={studyLevels}
+                    ></Select>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FileUploader name="resumePdf" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ButtonComponent>Apply</ButtonComponent>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default FormCandidature;
